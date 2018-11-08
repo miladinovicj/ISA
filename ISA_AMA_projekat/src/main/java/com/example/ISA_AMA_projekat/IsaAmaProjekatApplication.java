4efb027e348dc8dada7d2317f7586656492ffdfa
Milica Makaric <makaric.milica@uns.ac.rs>
2018-11-08 12:11:57 +0100

    inicijalni commit

---
+++ ISA_AMA_projekat/src/main/java/com/example/ISA_AMA_projekat/IsaAmaProjekatApplication.java
@@ -0,0 +1,12 @@
+package com.example.ISA_AMA_projekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaAmaProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaAmaProjekatApplication.class, args);
+	}
+}
