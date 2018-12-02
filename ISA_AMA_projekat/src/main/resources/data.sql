4bd2075897d4db01ebe21f65e4f4aa793caf8538
Milica Makaric <makaric.milica@uns.ac.rs>
2018-12-02 15:56:17 +0100

    izmene u data.sql

--- ISA_AMA_projekat/src/main/resources/data.sql
+++ ISA_AMA_projekat/src/main/resources/data.sql
@@ -1,3 +1,2 @@
-
 insert into hotel (naziv, adresa, promotivni_opis, prosecna_ocena) values ('Hotel Park', 'Novosadskog sajma 35 Novi Sad', 'hotel sa 4 zvezdice', 4.8);
 insert into hotel (naziv, adresa, promotivni_opis, prosecna_ocena) values ('Hotel Novi Sad', 'Jase Tomica 2 Novi Sad', 'fantastican hotel', 3);
