3257efac3334e04b2e563bac2a50b53587dc4265
AndrijanaJeremic <44624036+AndrijanaJeremic@users.noreply.github.com>
2019-01-04 23:05:35 +0100

    Pocetna stranica za registrovanog korisnika

--- ISA_AMA_projekat/src/main/java/com/example/ISA_AMA_projekat/controller/KorisnikController.java
+++ ISA_AMA_projekat/src/main/java/com/example/ISA_AMA_projekat/controller/KorisnikController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.ISA_AMA_projekat.model.Authority;
 import com.example.ISA_AMA_projekat.model.FriendRequest;
 import com.example.ISA_AMA_projekat.model.Korisnik;
 import com.example.ISA_AMA_projekat.model.Poziv;
