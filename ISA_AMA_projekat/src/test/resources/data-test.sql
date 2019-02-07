595b89061a8bf2b0ef3aff85bbceec3f6bd96c7e
AndrijanaJeremic <44624036+AndrijanaJeremic@users.noreply.github.com>
2019-02-08 00:04:39 +0100

    Testovi

---
+++ ISA_AMA_projekat/src/test/resources/data-test.sql
@@ -0,0 +1,3 @@
+insert into hotel (id, naziv, promotivni_opis, prosecna_ocena, id_admin) values (2, 'Hotel 1', 'prvi hotel',4, 1);
+insert into hotel (id, naziv, promotivni_opis, prosecna_ocena, id_admin) values (1, 'Petar', 'Petrovic', 3.5, 2);
+insert into hotel (id, naziv, promotivni_opis, prosecna_ocena, id_admin) values (3, 'Hotel 2', 'drugi hotel', 3, 3);
